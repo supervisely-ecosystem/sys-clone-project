a97165f0a373beb5961b4bd0ee6ba1c6f2514595
cxnt <pavelbartsits@gmail.com>
2022-10-25 20:26:45 +0400

    clone project/dataset WIP

--- src/globals.py
+++ src/globals.py
@@ -12,19 +12,19 @@ sly.logger.info(f'PYTHONPATH={os.environ.get("PYTHONPATH", "")}')
 
 # order matters
 from dotenv import load_dotenv
-load_dotenv(os.path.join(app_root_directory, "secret_debug.env"))
-load_dotenv(os.path.join(app_root_directory, "debug.env"))
+load_dotenv(os.path.join(app_root_directory, "sys-clone-project", "secret_debug.env"))
+load_dotenv(os.path.join(app_root_directory, "sys-clone-project", "debug.env"))
 
 api: sly.Api = sly.Api.from_env()
 my_app: AppService = AppService()
 
+TASK_ID = os.environ.get("TASK_ID", None)
 
 TEAM_ID = os.environ.get("CONTEXT_TEAMID", None)
 WORKSPACE_ID = os.environ.get("CONTEXT_WORKSPACEID", None)
 PROJECT_ID = os.environ.get("CONTEXT_PROJECTID", None)
 DATASET_ID = os.environ.get("CONTEXT_DATASETID", None)
 
-DEST_TEAM_ID = os.environ.get("modal.state.teamId", None)
 DEST_WORKSPACE_ID = os.environ.get("modal.state.workspaceId", None)
 DEST_PROJECT_ID = os.environ.get("modal.state.projectId", None)
 
