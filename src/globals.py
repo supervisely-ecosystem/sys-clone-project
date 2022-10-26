66b74d016274f63fac2119bb7e54d758ee336da5
cxnt <pavelbartsits@gmail.com>
2022-10-26 16:58:48 +0400

    update icon, remove output project, remove unnecessary lines

--- src/globals.py
+++ src/globals.py
@@ -2,7 +2,6 @@ import os
 import sys
 import supervisely as sly
 from supervisely.app.v1.app_service import AppService
-from distutils import util
 
 app_root_directory = os.path.dirname(os.getcwd())
 sys.path.append(app_root_directory)
@@ -11,9 +10,9 @@ print(f"App root directory: {app_root_directory}")
 sly.logger.info(f'PYTHONPATH={os.environ.get("PYTHONPATH", "")}')
 
 # order matters
-from dotenv import load_dotenv
-load_dotenv(os.path.join(app_root_directory, "sys-clone-project", "secret_debug.env"))
-load_dotenv(os.path.join(app_root_directory, "sys-clone-project", "debug.env"))
+# from dotenv import load_dotenv
+# load_dotenv(os.path.join(app_root_directory, "sys-clone-project", "secret_debug.env"))
+# load_dotenv(os.path.join(app_root_directory, "sys-clone-project", "debug.env"))
 
 api: sly.Api = sly.Api.from_env()
 my_app: AppService = AppService()
@@ -30,6 +29,3 @@ DEST_PROJECT_ID = os.environ.get("modal.state.projectId", None)
 
 PROJECT_NAME = os.environ.get("modal.state.projectName", None)
 DATASET_NAME = os.environ.get("modal.state.datasetName", None)
-
-if DATASET_ID:
-    DEST_DATASET_ID = os.environ.get("modal.state.datasetId", None)
