9f694ad72a84e2dfece32393f6d018a123730155
Pavel Bartsits <48913536+cxnt@users.noreply.github.com>
2022-10-28 12:33:10 +0400

    clone dataset correctly (#2)
    * ignore task id
    
    * clone dataset correctly
    
    * correct output name

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ import project_type.volume as volume
 import project_type.pointcloud as pointcloud
 import project_type.pointcloud_episodes as pointcloud_episodes
 
+
 @g.my_app.callback("clone-data")
 @sly.timeit
 def clone_data(api: sly.Api, task_id, context, state, app_logger):
@@ -14,22 +15,22 @@ def clone_data(api: sly.Api, task_id, context, state, app_logger):
     project_meta_json = api.project.get_meta(project.id)
     project_meta = sly.ProjectMeta.from_json(data=project_meta_json)
     project_type = project_meta.project_type
-    dst_project_name = g.PROJECT_NAME or project.name
 
     if not g.DEST_PROJECT_ID:
         dst_project = api.project.create(
             workspace_id=g.DEST_WORKSPACE_ID,
-            name=dst_project_name,
+            name=g.PROJECT_NAME or project.name,
             type=project.type,
             description=project.description,
             change_name_if_conflict=True,
         )
-
         api.project.update_meta(id=dst_project.id, meta=project_meta)
     else:
-        dst_project = project
-
-
+        dst_project = api.project.get_info_by_id(g.DEST_PROJECT_ID)
+        api.project.merge_metas(
+            src_project_id=project.id, dst_project_id=dst_project.id
+        )
+        
     if g.DATASET_ID:
         datasets = [api.dataset.get_info_by_id(g.DATASET_ID)]
     else:
@@ -38,17 +39,39 @@ def clone_data(api: sly.Api, task_id, context, state, app_logger):
     if project_type == str(sly.ProjectType.IMAGES):
         image.clone(api=api, project_id=dst_project.id, datasets=datasets)
     elif project_type == str(sly.ProjectType.VIDEOS):
-        video.clone(api=api, project_id=dst_project.id, datasets=datasets, project_meta=project_meta)
+        video.clone(
+            api=api,
+            project_id=dst_project.id,
+            datasets=datasets,
+            project_meta=project_meta,
+        )
     elif project_type == str(sly.ProjectType.VOLUMES):
-        volume.clone(api=api, project_id=dst_project.id, datasets=datasets, project_meta=project_meta)
+        volume.clone(
+            api=api,
+            project_id=dst_project.id,
+            datasets=datasets,
+            project_meta=project_meta,
+        )
     elif project_type == str(sly.ProjectType.POINT_CLOUDS):
-        pointcloud.clone(api=api, project_id=dst_project.id, datasets=datasets, project_meta=project_meta)
+        pointcloud.clone(
+            api=api,
+            project_id=dst_project.id,
+            datasets=datasets,
+            project_meta=project_meta,
+        )
     elif project_type == str(sly.ProjectType.POINT_CLOUD_EPISODES):
-        pointcloud_episodes.clone(api=api, project_id=dst_project.id, datasets=datasets, project_meta=project_meta)
+        pointcloud_episodes.clone(
+            api=api,
+            project_id=dst_project.id,
+            datasets=datasets,
+            project_meta=project_meta,
+        )
     else:
         raise NotImplementedError(f"Unknown project type: {project_type}")
 
-    api.app.set_output_project(task_id=g.TASK_ID, project_id=dst_project.id, project_name=dst_project_name)
+    api.app.set_output_project(
+        task_id=g.TASK_ID, project_id=dst_project.id, project_name=dst_project.name
+    )
     g.my_app.stop()
 
 
