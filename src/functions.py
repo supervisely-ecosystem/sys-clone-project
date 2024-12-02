62756d28fbf30f01969eb22c23a08e11e45e8383
almaz <almazgimaev@gmail.com>
2024-12-02 20:46:10 +0100

    fix typo

--- src/functions.py
+++ src/functions.py
@@ -3,7 +3,7 @@ def get_datasets_tree_msg(datasets_tree):
     def _collect_ds_tree_names(ds_tree, level=0):
         msg = ""
         for ds, nested_ds_tree in ds_tree.items():
-            msg = "  " * level + f"― {ds.name}\n"
+            msg += "  " * level + f"― {ds.name}\n"
             msg += _collect_ds_tree_names(nested_ds_tree, level + 1)
         return msg
 
