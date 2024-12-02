b747df4485785f2e807c305e1560baa12545a261
almaz <almazgimaev@gmail.com>
2024-12-02 20:54:53 +0100

    small fix

--- src/functions.py
+++ src/functions.py
@@ -1,10 +1,10 @@
 def get_datasets_tree_msg(datasets_tree):
 
     def _collect_ds_tree_names(ds_tree, level=0):
-        msg = "\n"
+        msg = ""
         for ds, nested_ds_tree in ds_tree.items():
             msg += "  " * level + f"― {ds.name}\n"
             msg += _collect_ds_tree_names(nested_ds_tree, level + 1)
         return msg
 
-    return _collect_ds_tree_names(datasets_tree)
+    return "\n" + _collect_ds_tree_names(datasets_tree)
